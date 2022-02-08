78a7720c4a23ee5c7c2772ea16f43e2bca23548a
Djepse20 <djepse20@student.aau.dk>
2022-02-08 02:07:22 +0100

    changed foreach and created a enumrable object

--- Foreach.c
+++ Foreach.c
@@ -14,54 +14,65 @@ typedef enum LIST_TYPES{
 #define XSTR(x) STR(x)
 #define STR(x) #x
 
-
+typedef struct list list;
 typedef struct array {
-    BYTE* Arr;
-
-    size_t Length;
-    size_t ElementSize;
+    BYTE* data;
 }array;
 
+struct list {
+    BYTE* data;
+    list* next;
+    list* curr;
+};
+typedef struct Enumerable{
+    union 
+    {
+        array* Arr;
+        list* list;
+    };
+    LIST_TYPES Type;
+    size_t Length;
+    size_t ElementSize;
+}Enumerable;
+
+
+
 #define ISCHAR(x) _Generic((x), char*: NOTVALID, default:x)
-#define TYPEOF(x) _Generic((x), struct array*: ARR)
 
 
-#define foreach_arr(ITEM, ARRAY,BODY) \
-    for(int count = 0, size = ARRAY->Length; count < size; count++)\
-    {\
-        ITEM = (void*)&(*((ARRAY->Arr)+count*ARRAY->ElementSize)); \
-        do\
-        BODY\
-        while(0);\
+
+#define foreach(ITEM, ENUMERABLE) \
+    for(int count = 0, size = ENUMERABLE->Length, keep = TRUE; keep && count < size; count++, keep = !keep)\
+        for(ITEM = GetNextElement((ENUMERABLE),count); keep; keep = FALSE)
+         
+
+
+
+void* GetNextElement(Enumerable*Enumerable,size_t count)
+{
+    BYTE* data;
+    switch (Enumerable->Type)
+    {
+    case ARR:
+        return (void*)&(*((Enumerable->Arr->data)+count*Enumerable->ElementSize));
+    case LIST:
+    //YES
+        if(count == 0)
+        {
+            Enumerable->list->curr = Enumerable->list->next;
+            return (void*)&(*(Enumerable->list->data));
+        }
+        data = Enumerable->list->curr->data;
+        Enumerable->list->curr = Enumerable->list->curr->next;
+        return (void*)&(*(data));
+        break;
     }
-        
-#define foreach_list(ITEM, LIST,BODY)\
-    for( void* item = list->head;  && item != NULL; item = item->next)\
-    {\
-        ITEM = item;\
-        do\
-        BODY\
-        while(0);\
-    }
-
-
-
-#define foreach(item, Enumerable,BODY) \
-    do\
-    {\
-        switch (TYPEOF(Enumerable))\
-        {\
-            case ARR:\
-            foreach_arr(item, Enumerable,BODY)\
-            \
-            break;\
-        }\
-    }while(0)
-
+    return NULL;
+}
 #define InsertIntoEnumerable(Enumerable,TYPE,...)\
     do\
     {\
-      switch (TYPEOF(Enumerable))\
+      switch (Enumerable->Type)\
       {\
             case ARR:\
             InsertIntoArr(Enumerable,sizeof((TYPE){0})/sizeof(*(TYPE){0}),(BYTE*)(TYPE){__VA_ARGS__}, (sizeof((BYTE[]){__VA_ARGS__})/sizeof(BYTE)));\
@@ -81,45 +92,47 @@ int ChunkEmpty(BYTE* initValues,size_t ElementSize)
 
     return TRUE;
 }
-void* FillArray(void* dest,array* arr, size_t Elements,void* src)
+void* FillArray(void* dest,Enumerable* arr, size_t Elements,void* src)
 {
+    
     memcpy(dest,src,Elements*arr->ElementSize);
     return dest;
 }
-void InsertIntoArr(array* array,size_t NumOfElements,void* elements,size_t elementsToInit)
+void InsertIntoArr(Enumerable* array,size_t NumOfElements,void* elements,size_t elementsToInit)
 {       
 
     size_t sizeOfArr = array->Length*array->ElementSize;
     BYTE* newArray = calloc(sizeOfArr+NumOfElements,array->ElementSize);
     size_t origLength = array->Length;
     array->Length += NumOfElements;
-    FillArray(newArray,array,origLength,array->Arr);
+    FillArray(newArray,array,origLength,array->Arr->data);
 
     if(elementsToInit  > 1 || !ChunkEmpty(elements,array->ElementSize))  
         FillArray(newArray+(array->ElementSize*(origLength-1)),array,elementsToInit,elements);
 
-    free(array->Arr);
-    array->Arr = newArray;
 
-
+    free(array->Arr->data);
+    array->Arr->data = newArray;
 }
 
 
-void* FillElementsInArray(array* arr,void* initValues, size_t numOfInits)
+void* FillElementsInArray(Enumerable* arr,void* initValues, size_t numOfInits)
 {
     if(numOfInits == 1 && ChunkEmpty(initValues,arr->ElementSize))
         return arr->Arr;
-
-    return FillArray(arr->Arr,arr,numOfInits,initValues);
+    return FillArray(arr->Arr->data,arr,numOfInits,initValues);
 }
-array* CreateArray(size_t element_size, size_t length,BYTE* initValues, size_t numOfInits)
+Enumerable* CreateArray(size_t element_size, size_t length,BYTE* initValues, size_t numOfInits)
 {
-    array* arr = malloc(sizeof(arr));
-    arr->Arr = calloc(length,element_size);    
-
+    
+    Enumerable* arr = malloc(sizeof(Enumerable));
+    arr->Arr = malloc(sizeof(Enumerable));
+    arr->Arr->data = calloc(length,element_size);    
+    arr->Type = ARR;
     arr->Length = length;
     arr->ElementSize = element_size;
-    arr->Arr = FillElementsInArray(arr,initValues,numOfInits);
+    
+    arr->Arr->data = FillElementsInArray(arr,initValues,numOfInits);
 
     return arr;
 }
@@ -129,16 +142,15 @@ int main(void)
 {
     // creates new array of specified TYPE and size, with a list of elements to initialize afterwards
     //ALWAYS USE Type[numElem] and not a Type*.
-    //The latter will make an array of 8 bytes, the first numElem bytes.
+    //The latter will make an   of 8 bytes, the first numElem bytes.
     //using Type* will therfore as a consequence break foreach :) 
     // a fix that could (and proably should be implented would be the need to specify the type and size)
-    array* arr = NewArray(char[20],"mus seee");
+    Enumerable* arr = NewArray(char[20],"test1");
+    
     //foreach variable v of TYPE T in array, do this
-    
-    
-    foreach(char* v,arr, 
+    InsertIntoEnumerable(arr,char[20],"test2");
+    foreach(char* v,arr)
         printf("%c",*v);
-    );
     return 0;
 }
 
